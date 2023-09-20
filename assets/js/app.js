1ab2e3e304e7cacb5fbd31e3ced4491e55b7b957
TapashIT07 <tapashit07@gmail.com>
2023-09-20 17:54:36 +0600

    fifth commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,4 +4,24 @@ $(function(){
         $('.menu').slideToggle();
     });
 
+$('.back_to_top').click(function(){
+    $('html,body').animate({scrollTop:0},500);
+
+});
+
+
+$(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+
+    if (scrollTop>400){
+        $('.back_to_top').show();
+    } else{
+        
+        $('.back_to_top').hide();
+
+    }
+});
+
+
+
 });
